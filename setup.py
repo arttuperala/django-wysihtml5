911178f7748df5386d45e60f47761470efc8ecfa
Arttu Perälä <arttu@perala.me>
2023-02-03 08:17:31 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ test.run_tests = run_tests
 
 setup(
     name = "django-wysihtml5",
-    version = "1.2b3.0",
+    version = "2.0.0",
     packages = find_packages(),
     include_package_data = True,
     license = "MIT",
